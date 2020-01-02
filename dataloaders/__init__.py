9c75bf75bd21ea7c2e07aa20563bfbcf53c4442d
TWSFar <twsfcz@163>
2020-01-02 22:38:38 +0800

    update

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 def make_data_loader(opt, mode="train"):
 
     if opt.dataset in ['visdrone', 'VisDrone', 'Visdrone']:
-        dataset = VisDroneRegion(opt, mode="train")
+        dataset = VisDroneRegion(opt, mode)
         dataloader = DataLoader(dataset,
                                 batch_size=opt.batch_size,
                                 num_workers=opt.workers,
