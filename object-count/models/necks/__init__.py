11fe887c004ecb1f4da90af5a34328c2291fa851
TWSFar <twsfcz@163.com>
2020-05-09 10:04:33 +0800

    update

--- object-count/models/necks/__init__.py
+++ object-count/models/necks/__init__.py
@@ -2,7 +2,8 @@ from .aspp import ASPP
 from .selayer import SELayer
 from .inception import Inception
 from .gcblock import ContextBlock2d
+from .rfb import BasicRFB_a, BasicRFB
 
 __all__ = [
-    "ASPP", "SELayer", "Inception", 'ContextBlock2d'
+    "ASPP", "SELayer", "Inception", 'ContextBlock2d', 'BasicRFB_a', 'BasicRFB'
 ]
