471e94f42fd8714011f945fe22f6b10cc7c617b8
TWSFar <twsfcz@163.com>
2020-06-15 20:57:11 +0800

    update

--- mmdetection/tools_tt100k/configs/density/ATSS_x101_fpn_giou.py
+++ mmdetection/tools_tt100k/configs/density/ATSS_x101_fpn_giou.py
@@ -1,6 +1,8 @@
 _base_ = [
-    '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    # '../_base_/datasets/coco_detection.py',
+    '../_base_/datasets/voc0712.py',
+    '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
 ]
 model = dict(
     type='ATSS',
